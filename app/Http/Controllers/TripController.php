f4fff5acefc794ef5e47f540ffffa3d67dad57df
Douglas Lundy <douglaslundy@gmail.com>
2025-03-19 11:19:43 -0300

    fixed  edit cliente on trip function

--- app/Http/Controllers/TripController.php
+++ app/Http/Controllers/TripController.php
@@ -238,6 +238,12 @@ class TripController extends Controller
     //     }
     // }
 
+     /**
+     * Confirma a viagem de um cliente.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function editTripClient(TripClientRequest $request, $id)
 {
     DB::beginTransaction();
