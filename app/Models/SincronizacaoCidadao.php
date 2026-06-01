a6753a8949ef119ba11a7c839b2151cd19cd2c65
Douglas Lundy <douglaslundy@gmail.com>
2026-05-31 21:08:07 -0300

    feat: model SincronizacaoCidadao

--- app/Models/SincronizacaoCidadao.php
+++ app/Models/SincronizacaoCidadao.php
@@ -11,7 +11,7 @@ class SincronizacaoCidadao extends Model
     protected $table = 'sincronizacoes_cidadao';
 
     protected $fillable = [
-        'job_id', 'status', 'total_esus', 'total_sysdoc',
+        'job_id', 'status', 'total_esus', 'total_esus_previsto', 'total_sysdoc',
         'preview_criados', 'preview_atualizados', 'preview_obitos', 'preview_sem_alteracao',
         'result_criados', 'result_atualizados', 'result_obitos', 'result_erros',
         'iniciado_por', 'aplicado_por', 'analisado_em', 'aplicado_em', 'erro_mensagem',
